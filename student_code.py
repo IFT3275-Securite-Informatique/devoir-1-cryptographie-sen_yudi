372e7e00073b16dc0593240fd772de789b44c3b0
Sennysen <113637629+Sennysen@users.noreply.github.com>
2024-11-03 01:13:13 -0400

    Revert "premier essaie"
    This reverts commit c8dec39c37a8dee19ab8e427b058a626cf821bbe.

--- student_code.py
+++ student_code.py
@@ -1,44 +1,5 @@
-import random as rnd
-def split_into_blocs(binary_text):
-    return [binary_text[i:i + 8] for i in range(0, len(binary_text), 8)]
-
-def gen_key(symboles):
-  l=len(symboles)
-  if l > 256:
-    return False
-
-  rnd.seed(1337)
-  int_keys = rnd.sample(list(range(l)),l)
-  dictionary = dict({})
-  for s,k in zip(symboles,int_keys):
-    dictionary[s]="{:08b}".format(k )
-  return dictionary
-
-def decrypt_text(binary_blocks, mapping):
-    binary_to_symbol = {key: value for key, value in mapping.items()}
-    decrypted = "".join(binary_to_symbol.get(block, '') for block in binary_blocks)
-    return decrypted
-
 def decrypt(C):
-    symboles = ['b', 'j', '\r', 'J', '”', ')', 'Â', 'É', 'ê', '5', 't', '9', 'Y', '%', 'N', 'B', 'V', '\ufeff', 'Ê', '?',
-              '’', 'i', ':', 's', 'C', 'â', 'ï', 'W', 'y', 'p', 'D', '—', '«', 'º', 'A', '3', 'n', '0', 'q', '4', 'e',
-              'T', 'È', '$', 'U', 'v', '»', 'l', 'P', 'X', 'Z', 'À', 'ç', 'u', '…', 'î', 'L', 'k', 'E', 'R', '2', '_',
-              '8', 'é', 'O', 'Î', '‘', 'a', 'F', 'H', 'c', '[', '(', "'", 'è', 'I', '/', '!', ' ', '°', 'S', '•', '#',
-              'x', 'à', 'g', '*', 'Q', 'w', '1', 'û', '7', 'G', 'm', '™', 'K', 'z', '\n', 'o', 'ù', ',', 'r', ']', '.',
-              'M', 'Ç', '“', 'h', '-', 'f', 'ë', '6', ';', 'd', 'ô', 'e ', 's ', 't ', 'es', ' d', '\r\n', 'en', 'qu',
-              ' l', 're', ' p', 'de', 'le', 'nt', 'on', ' c', ', ', ' e', 'ou', ' q', ' s', 'n ', 'ue', 'an', 'te',
-              ' a', 'ai', 'se', 'it', 'me', 'is', 'oi', 'r ', 'er', ' m', 'ce', 'ne', 'et', 'in', 'ns', ' n', 'ur',
-              'i ', 'a ', 'eu', 'co', 'tr', 'la', 'ar', 'ie', 'ui', 'us', 'ut', 'il', ' t', 'pa', 'au', 'el', 'ti',
-              'st', 'un', 'em', 'ra', 'e,', 'so', 'or', 'l ', ' f', 'll', 'nd', ' j', 'si', 'ir', 'e\r', 'ss', 'u ',
-              'po', 'ro', 'ri', 'pr', 's,', 'ma', ' v', ' i', 'di', ' r', 'vo', 'pe', 'to', 'ch', '. ', 've', 'nc',
-              'om', ' o', 'je', 'no', 'rt', 'à ', 'lu', "'e", 'mo', 'ta', 'as', 'at', 'io', 's\r', 'sa', "u'", 'av',
-              'os', ' à', ' u', "l'", "'a", 'rs', 'pl', 'é ', '; ', 'ho', 'té', 'ét', 'fa', 'da', 'li', 'su', 't\r',
-              'ée', 'ré', 'dé', 'ec', 'nn', 'mm', "'i", 'ca', 'uv', '\n\r', 'id', ' b', 'ni', 'bl']
-
-    binary_blocks = split_into_blocs(C)
-    dictionary = gen_key(symboles)
-
-    mapping = {value: key for key, value in dictionary.items()}
-    M = decrypt_text(binary_blocks, mapping)
-
-    return M
+  M=""
+  #entrez votre code ici.
+  #Vous pouvez créer des fonctions auxiliaires et adapter le code à votre façon mais decrypt dois renvoyer le message décrypté
+  return M
--- test3.py
+++ test3.py
@@ -1,5 +1,4 @@
 import random
-import time
 import unittest
 from difflib import SequenceMatcher, unified_diff
 from crypt import *
